a75676deef3e99e679357a772a8f3f1a7cf2665d
Filgy <filgy@sniff.cz>
2012-01-27 23:24:02 +0100

    Implemented main - User interface

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+#include "watchdog.h"
+
+string HELP_MESSAGE = "";
+
+int main(int argc, char* argv[]){
+    if(argc == 2 && strcmp("--help", argv[1]) == 0)
+        cout << HELP_MESSAGE << endl;
+    else{
+        try{
+            watchdog puppy("./callbacks/");
+        }
+        catch(...){
+            cerr << "Got exception" << endl;
+        }
+    }
+
+    return 0;
+}
