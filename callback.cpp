ebdba5076f9db6d1f76984df3e7fb6db2db1ebf6
Filgy <filgy@sniff.cz>
2012-01-27 23:22:20 +0100

    Implemented callback - class represents callback data

---
+++ callback.cpp
@@ -0,0 +1,66 @@
+#include "callback.h"
+
+trigger::trigger(string name, string pattern, string command){
+    this->name = name;
+    this->pattern = pattern;
+    this->command = command;
+}
+
+string trigger::getName(){
+    return this->name;
+}
+
+string trigger::getPattern(){
+    return this->pattern;
+}
+
+string trigger::getCommand(){
+    return this->command;
+}
+
+callback::callback(string name, string logfile, unsigned int position, unsigned int size)
+{
+    this->name = name;
+    this->logfile = logfile;
+    this->position = position;
+    this->size = size;
+    this->triggers.clear();
+}
+
+void callback::setPosition(unsigned int position){
+    this->position = position;
+}
+
+void callback::setSize(unsigned int size){
+    this->size = size;
+}
+
+string callback::getName(){
+    return this->name;
+}
+
+string callback::getLogfile(){
+    return this->logfile;
+}
+
+unsigned int callback::getPosition(){
+    return this->position;
+}
+
+unsigned int callback::getSize(){
+    return this->size;
+}
+
+int callback::getTriggersCount(){
+    return this->triggers.size();
+}
+
+void callback::addTrigger(string name, string pattern, string command){
+    trigger tmp(name, pattern, command);
+
+    this->triggers.insert(make_pair(name, tmp));
+}
+
+map<string, trigger> callback::getTriggers(){
+    return this->triggers;
+}
---
+++ callback.h
@@ -0,0 +1,54 @@
+#ifndef CALLBACK_H
+#define CALLBACK_H
+
+#include <string>
+#include <map>
+
+using namespace std;
+
+class trigger
+{
+    public:
+        trigger(string, string, string);
+
+        string getName();
+        string getPattern();
+        string getCommand();
+
+    private:
+        string name;
+        string pattern;
+        string command;
+};
+
+class callback
+{
+    public:
+        callback(string, string, unsigned int = 0, unsigned int = 0);
+
+        void setPosition(unsigned int);
+        void setSize(unsigned int);
+
+        string getName();
+        string getLogfile();
+        unsigned int getPosition();
+        unsigned int getSize();
+        int getTriggersCount();
+
+        void addTrigger(string, string, string);
+
+        map<string, trigger> getTriggers();
+    protected:
+    private:
+        string name;
+        string logfile;
+        unsigned int position;
+        unsigned int size;
+
+        map<string, trigger> triggers;
+
+};
+
+
+
+#endif // CALLBACK_H
