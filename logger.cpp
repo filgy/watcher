563ca6690ec9fc5a7b65bb9433b9cfb7b2a8107b
Filgy <filgy@sniff.cz>
2012-01-28 11:51:49 +0100

    Implemented class logger

---
+++ logger.cpp
@@ -0,0 +1,58 @@
+#include "logger.h"
+
+logger* logger::instance = NULL;
+fstream* logger::fileHandler = NULL;
+
+logger* logger::getInstance(){
+    if(!instance)
+        instance = new logger;
+
+    return instance;
+}
+
+bool logger::open(string fileName){
+    logger::fileHandler = new fstream(fileName.c_str(), ios::out);
+
+    if(logger::fileHandler->is_open())
+        return true;
+    else{
+        delete logger::fileHandler;
+        return false;
+    }
+}
+
+bool logger::write(string message, bool verbose){
+    if(verbose)
+        cout << "[*] " << message << endl;
+
+    if(logger::fileHandler && logger::fileHandler->is_open()){
+        *(logger::fileHandler) << "[*] " << message << endl;
+        return true;
+    }
+
+    return false;
+}
+
+bool logger::error(string message, bool verbose){
+    if(verbose)
+        cerr << "[!] " << message << endl;
+
+    if(logger::fileHandler && logger::fileHandler->is_open()){
+        *(logger::fileHandler) << "[!] " << message << endl;
+        return true;
+    }
+
+    return false;
+}
+
+bool logger::close(){
+    if(logger::fileHandler && logger::fileHandler->is_open()){
+        logger::fileHandler->close();
+        delete logger::fileHandler;
+        return true;
+    }
+
+    return false;
+}
+
+
---
+++ logger.h
@@ -0,0 +1,28 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class logger
+{
+    public:
+        static logger* getInstance();
+        bool open(string);
+        bool write(string, bool = false);
+        bool error(string, bool = false);
+        bool close();
+
+    private:
+        logger(){};
+        logger(logger const&){};
+        logger& operator =(logger const&){};
+
+        static logger* instance;
+        static fstream* fileHandler;
+};
+
+#endif // LOGGER_H
