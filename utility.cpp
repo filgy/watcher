d96fefbbcd5e5959151959941be5b3b48cc7f462
Filgy <filgy@sniff.cz>
2012-01-29 18:05:08 +0100

    Cleaning class utility

--- utility.cpp
+++ utility.cpp
@@ -1,5 +1,8 @@
 #include "utility.h"
 
+/*  - - - - - - - - - - - - - - - - - - - - - -
+        Utilities for strings
+    ------------------------------------------- */
 string utility::replace(string search, string replace, string subject){
     size_t pos;
 
@@ -9,57 +12,60 @@ string utility::replace(string search, string replace, string subject){
     return subject;
 }
 
-string utility::trim(string str){
-    unsigned int start,end;
+string utility::trim(string subject){
+    size_t start,end;
 
-    for(start = 0; start < str.length() && str.at(start) == ' '; start++);
+    for(start = 0; start < subject.size() && subject.at(start) == ' '; start++);
+    for(end = subject.size(); subject.size() > 0 && end > 0 && subject.at(end - 1) == ' '; end--);
 
-    for(end = str.length(); str.length() > 0 && end > 0 && str.at(end - 1) == ' '; end--);
-
-    str = str.substr(start, end - start);
-
-    return str;
+    return subject.substr(start, end - start);
 }
 
+/*  - - - - - - - - - - - - - - - - - - - - - -
+        Utilities for conversion
+    ------------------------------------------- */
 string utility::ltos(long number){
-    ostringstream str;
-    str << number;
-    return str.str();
+    ostringstream stream;
+    stream << number;
+    return stream.str();
 }
 
 string utility::itos(int number){
-    ostringstream str;
-    str << number;
-    return str.str();
+    ostringstream stream;
+    stream << number;
+    return stream.str();
 }
 
 long utility::stol(string str){
-    istringstream is(str);
+    istringstream stream(str);
     long number;
-    is >> number;
+    stream >> number;
     return number;
 }
 
 int utility::stoi(string str){
-    istringstream is(str);
+    istringstream stream(str);
     int number;
-    is >> number;
+    stream >> number;
     return number;
 }
 
-long utility::fileSize(string fileName){
-    ifstream fileHandler(fileName.c_str());
+/*  - - - - - - - - - - - - - - - - - - - - - -
+        Utilities for files
+    ------------------------------------------- */
+long utility::fileSize(string filename){
+    long position;
+    ifstream fileHandler(filename.c_str());
 
     if(!fileHandler.is_open())
         return 0;
 
     fileHandler.seekg(0, ios::end);
 
-    long begin = 0;
-    long end = fileHandler.tellg();
+    position = fileHandler.tellg();
 
     fileHandler.close();
 
-    return (end - begin);
+    return position;
 }
 
--- utility.h
+++ utility.h
@@ -10,12 +10,17 @@ using namespace std;
 class utility
 {
     public:
+        //String
         static string replace(string, string, string);
         static string trim(string);
+
+        //Conversion
         static string ltos(long);
         static string itos(int);
         static long stol(string);
         static int stoi(string);
+
+        //Files
         static long fileSize(string);
 };
 
